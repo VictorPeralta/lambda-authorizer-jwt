8bd1dfeba696e99c2d732c7ca5d99cae78cc9603
vperaltar <peralta.r.victor@gmail.com>
2020-01-24 13:32:32 -0800

    refactor registration function

--- src/handlers/registration.js
+++ src/handlers/registration.js
@@ -7,57 +7,12 @@ var docClient = new aws.DynamoDB.DocumentClient();
 exports.register = async (event, context) => {
     try {
         const user = JSON.parse(event.body);
-
-        const params = {
-            TableName: "LambdaAuthUserTable",
-            Item: {
-                "Email": user.email,
-                "Password": await bcrypt.hash(user.password, 8)
-            }
-        };
-
-        await docClient.put(params).promise();
-
-        var email = {
-            Destination: {
-                ToAddresses: [user.email]
-            },
-            Message: {
-                Body: {
-                    Html: {
-                        Charset: "UTF-8",
-                        Data: `<h1>Welcome to lifeboard</h1>. <br>Hello ${user.email}, please confirm your address by clicking <a href='#'>here</a>`
-                    }
-                },
-                Subject: {
-                    Charset: 'UTF-8',
-                    Data: 'Account activation'
-                }
-            },
-            Source: 'peralta.r.victor@gmail.com', /* required */
-            ReplyToAddresses: [
-                'peralta.r.victor@gmail.com',
-                /* more items */
-            ],
-        };
-
-        aws.config.update({region: 'us-west-2'});
-        var sendPromise = await new aws.SES({apiVersion: '2010-12-01'}).sendEmail(email).promise();
-        console.log(sendPromise.MessageId)
-
-        const payload = {
-            sub: user.email,
-            iss: "test-app"
-        }
-
-        //A JWT is created with the payload content
-        const token = jwt.sign(payload, "secret")
+        const token = await registerUserGetToken(user);
 
         return {
             statusCode: 200,
             body: JSON.stringify(token) //JWT is sent to API caller
         };
-
     } catch (error) {
         console.log(error);
         return {
@@ -65,6 +20,64 @@ exports.register = async (event, context) => {
             body: error.message
         };
     }
+}
 
+const registerUserGetToken = async (user) => {
+    try {
+        await addUserToDB(user);  
+        await sendRegistrationEmail(user);
+        return signToken(user);
+        
+    } catch (error) {
+        throw error;
+    }   
+}
 
+const addUserToDB = async (user) => {
+    const params = {
+        TableName: "LambdaAuthUserTable",
+        Item: {
+            "Email": user.email,
+            "Password": await bcrypt.hash(user.password, 8)
+        }
+    };
+    docClient.put(params).promise();
+}
+
+const sendRegistrationEmail = async (user) => {
+    var email = {
+        Destination: {
+            ToAddresses: [user.email]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: `<h1>Welcome to lifeboard</h1>. <br>Hello ${user.email}, please confirm your address by clicking <a href='#'>here</a>`
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Account activation'
+            }
+        },
+        Source: 'peralta.r.victor@gmail.com', /* required */
+        ReplyToAddresses: [
+            'peralta.r.victor@gmail.com',
+            /* more items */
+        ],
+    };
+
+    aws.config.update({region: 'us-west-2'});
+    new aws.SES({apiVersion: '2010-12-01'}).sendEmail(email).promise();
+}
+
+const signToken = (user) => {
+    const payload = {
+        sub: user.email,
+        iss: "test-app"
+    }
+
+    //A JWT is created with the payload content
+    return jwt.sign(payload, "secret")
 }
