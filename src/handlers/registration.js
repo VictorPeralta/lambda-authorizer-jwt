e4bb9b21f2483ca2ea5d27b9612bc1ef4af4dd52
vperaltar <vperaltar@tress.com.mx>
2020-01-23 17:52:06 -0800

    Added email verification functionality

--- src/handlers/registration.js
+++ src/handlers/registration.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 var aws = require('aws-sdk');
 var docClient = new aws.DynamoDB.DocumentClient();
 
+
 exports.register = async (event, context) => {
     try {
         const user = JSON.parse(event.body);
@@ -17,6 +18,38 @@ exports.register = async (event, context) => {
 
         await docClient.put(params).promise();
 
+        var params = {
+            Destination: {
+                ToAddresses: [user.email]
+            },
+            Message: {
+                Body: {
+                    Html: {
+                        Charset: "UTF-8",
+                        Data: `<h1>Welcome to lifeboard</h1>. <br>Hello ${user.email}, please confirm your address by clicking <a href='#'>here</a>`
+                    }
+                },
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Account activation'
+                }
+            },
+            Source: 'peralta.r.victor@gmail.com', /* required */
+            ReplyToAddresses: [
+                'peralta.r.victor@gmail.com',
+                /* more items */
+            ],
+        };
+
+
+        var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+        sendPromise.then(
+            function(data) {
+              console.log(data.MessageId);
+            }).catch(
+              function(err) {
+              console.error(err, err.stack);
+            });
 
         const payload = {
             sub: user.email,
